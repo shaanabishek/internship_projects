0a311c02d2f6f8c778896ed79ad56060f4d450e6
shaanabishek <fulcrum1234567890@gmail.com>
2026-07-02 11:14:33 +0530

    Add files via upload

---
+++ 1st_project.py
@@ -0,0 +1,11 @@
+name = "Shaan Abishek"                 
+age = 20                       
+learning = "Python"            
+has_coded_before = True         
+goal = "AI Developer"  
+
+print(f"{'Name':<15}: {name}")
+print(f"{'Age':<15}: {age}")
+print(f"{'Learning':<15}: {learning}")
+print(f"{'Has Coded Before?':<15}: {has_coded_before}")
+print(f"{'Goal':<15}: {goal}")
